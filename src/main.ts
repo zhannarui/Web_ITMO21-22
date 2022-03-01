6d961f4c130e99a02f52394e01263d2e270673fb
zhannarui <zhannik2001@gmail.com>
2022-03-02 00:48:33 +0300

    lab1 fixes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('pug');
-  const PORT = process.env.PORT || 49001;
-  await app.listen(PORT);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
-
 
 
 
