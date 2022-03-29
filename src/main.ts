67dd5e9b063400ac4290559ab3c8f5d88384c176
zhannarui <zhannik2001@gmail.com>
2022-03-29 18:27:49 +0300

    lab2

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.useStaticAssets(join(__dirname, '..', '/public'));
+  app.setBaseViewsDir(join(__dirname, '..', '/views'));
   app.setViewEngine('pug');
   const PORT = process.env.PORT || 12345;
   await app.listen(PORT);
